73b20f8bbc0b440c628591bd160bb92381517a8a
payloadskerim <82816014+payloadskerim@users.noreply.github.com>
2021-07-19 19:35:42 +0300

    Update calculus_machine.py

--- calculus_machine.py
+++ calculus_machine.py
@@ -1,8 +1,11 @@
 sonuc = ""
 
 islemKarar = True
-while islemKarar:
-    def islem_secim():
+while islemKarar:  """kodun bu kısmında işlemlerin bitip bitmediğini sürekli olarak kontrol etmek gerektiği için sonsuz bir 
+                      while döngüsü oluşturdum ancak bu döngü klavyeden "e" karakteri haricinde girelen bütün karakterlerde bozuluyor."""
+    
+    
+    def islem_secim(): # bu fonksiyon işlem seçimini ve klavyeden işlem yapılacak olan verileri alıp seçilen işleme göre sonucu ekrana yazdırmaktadır. 
         sayi1 = int(input("1. sayıyı giriniz : "))
 
         islem = input("lütfen (+) (-) (*) (/) işlemlerinden birini seçiniz : ")
@@ -26,7 +29,7 @@ while islemKarar:
             else:
                 sonuc = sayi1 / sayi2
                 print("Sonuç : ",sonuc )
-        def islem_gecmis_kayit():
+        def islem_gecmis_kayit(): # bu işlem ile daha önce yapılan işlemleri bir .txt dosyasına kaydederek daha sonra kullanılabilmesi veya bir geçmiş olarak kullanıcıya tekrar döndürlmesi sağlanmıştır.
             
             file = open("btk/YapayZeka/islem_gecmis.txt","a")
             file.write(" Isleminiz : " + str(sayi1) + " " + str(islem) + " " + str(sayi2) +"  "+"Islem Sonucunuz :" + str(sonuc)+"\n")
@@ -34,7 +37,8 @@ while islemKarar:
         islem_gecmis_kayit()
     islem_secim()
     islemDevam = input("Başka İşlem Yapacakmısınız ? (e/h) : ")
-    if islemDevam == "e":
+    
+    if islemDevam == "e": # uygulamanın kapatılıcak mı yoksa daha fazla işlem yapılıcak mı olduğunun kararını veren karar bloğu 
         pass
     else:
         print("İyi Günler :)")
